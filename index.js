dd045152cd93ce7d58ae681135b681f59b9c9674
Ralph Rouhana <ralph.rouhana@gmail.com>
2024-04-06 00:50:44 -0400

    Env

--- index.js
+++ index.js
@@ -1,42 +1,49 @@
-const express = require('express');
-const {MongoClient} = require('mongodb');
+const express = require("express");
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
 
-const uri = 'mongodb+srv://admin:zCzVtaAeRsQOYYj1@clothingsite.b0csd.mongodb.net/?retryWrites=true&w=majority';
+const uri = process.env.MONGODB_URI;
 const PORT = process.env.PORT || 4000;
 const app = express();
 
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });   
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-app.get('/', (req, res) => {
-    res.send("This works");
+app.get("/", (req, res) => {
+  res.send("This works");
 });
 
 async function sendData(res, type) {
-    try {
-        let productData = await client.db("siteData").collection("products").find({ productType: type }).toArray();
-        // Allow all origins
-        res.writeHead(200, {
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
-            'Access-Control-Allow-Headers': 'Content-Type',
-            'Access-Control-Allow-Credentials': true
-        });
-        res.end(JSON.stringify(productData));
-    } catch (e) {
-        console.error(e);
-    }
+  try {
+    let productData = await client
+      .db("siteData")
+      .collection("products")
+      .find({ productType: type })
+      .toArray();
+    // Allow all origins
+    res.writeHead(200, {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
+      "Access-Control-Allow-Headers": "Content-Type",
+      "Access-Control-Allow-Credentials": true,
+    });
+    res.end(JSON.stringify(productData));
+  } catch (e) {
+    console.error(e);
+  }
 }
 
-client.connect(err => {
-    if (err) {
-        console.log(err)
-    } else {
-        app.get('/:productType', (req, res) => {
-            sendData(res, req.params.productType);
-        });
-    }
+client.connect((err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.get("/:productType", (req, res) => {
+      sendData(res, req.params.productType);
+    });
+  }
 });
-
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
